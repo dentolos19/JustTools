ca8b6644f9c17f8bd7688d75bb8a045702374c08
Dennise Catolos <contact@dennise.me>
2020-09-03 15:40:07 +0800

    Updated project

--- CommonToolsOnline/Program.cs
+++ CommonToolsOnline/Program.cs
@@ -14,9 +14,7 @@ namespace CommonToolsOnline
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddScoped(provider => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             await builder.Build().RunAsync();
         }
 
