fdc8a258498fbbc22e9b95e940b284ec80b02d04
Dennise Catolos <contact@dennise.me>
2020-09-04 20:03:59 +0800

    Fixed issues

--- CommonToolsOnline/Forms/PasswordGeneratorForm.cs
+++ CommonToolsOnline/Forms/PasswordGeneratorForm.cs
@@ -6,7 +6,7 @@ namespace CommonToolsOnline.Forms
     public class PasswordGeneratorForm
     {
 
-        [Required]
+        [Required(ErrorMessage = "You must enter a password length.")]
         [Range(8, 64, ErrorMessage = "The length must be between 8 to 64 characters long.")]
         public int PasswordLength { get; set; } = 16;
 
