82cef0268dc11e52ccfff3300f518cbf132bc223
Dennise Catolos <contact@dennise.me>
2020-09-04 19:18:13 +0800

    Updated password generator

---
+++ CommonToolsOnline/Forms/PasswordGeneratorForm.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommonToolsOnline.Forms
+{
+
+    public class PasswordGeneratorForm
+    {
+
+        [Required]
+        [Range(8, 64, ErrorMessage = "The length must be between 8 to 64 characters long.")]
+        public int PasswordLength { get; set; } = 16;
+
+    }
+
+}
