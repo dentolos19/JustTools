39c48519360457c26636e3dac0982bfaa08199a9
Dennise Catolos <contact@dennise.me>
2020-09-05 09:58:48 +0800

    Added json formatter

---
+++ CommonToolsOnline/Forms/JsonFormatterForm.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommonToolsOnline.Forms
+{
+
+    public class JsonFormatterForm
+    {
+
+        [Required(ErrorMessage = "You must enter a JSON code to format.")]
+        public string Code { get; set; }
+
+    }
+
+}
