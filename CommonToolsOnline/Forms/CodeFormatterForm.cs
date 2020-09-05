0b7f863674c3a16ddc3c50429c03aaa2a8a8e539
Dennise Catolos <contact@dennise.me>
2020-09-05 13:47:33 +0800

    Added advanced features

---
+++ CommonToolsOnline/Forms/CodeFormatterForm.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommonToolsOnline.Forms
+{
+
+    public class CodeFormatterForm
+    {
+
+        [Required(ErrorMessage = "You must select a language to format.")]
+        public string Language { get; set; } = "JSON";
+
+        [Required(ErrorMessage = "You must specify JSON code to format.")]
+        public string Code { get; set; }
+
+    }
+
+}
--- CommonToolsOnline/Forms/PasswordGeneratorForm.cs
+++ CommonToolsOnline/Forms/PasswordGeneratorForm.cs
@@ -10,6 +10,16 @@ namespace CommonToolsOnline.Forms
         [Range(8, 64, ErrorMessage = "The password length must be only between 8 to 64 characters long.")]
         public int PasswordLength { get; set; } = 16;
 
+        public bool UseUppercaseAlphabets { get; set; } = true;
+
+        public bool UseLowercaseAlphabets { get; set; } = true;
+
+        public bool UseNumberCharacters { get; set; } = true;
+
+        public bool UseCustomCharacters { get; set; } = false;
+
+        public string CustomCharacters { get; set; } = @"!@#$%^&*?";
+
     }
 
 }
