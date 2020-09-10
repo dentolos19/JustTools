544a60bb8a6ca8642c3a70cff7ea99401612a3ca
Dennise Catolos <contact@dennise.me>
2020-09-10 12:12:11 +0800

    Updated text formatter

---
+++ CommonToolsOnline/Forms/TextFormatterForm.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommonToolsOnline.Forms
+{
+
+    public class TextFormatterForm
+    {
+
+        [Required(ErrorMessage = "You must select a format.")]
+        public string Format { get; set; } = "Reverse";
+
+        [Required(ErrorMessage = "You must specify text to format.")]
+        public string Text { get; set; }
+
+    }
+
+}
