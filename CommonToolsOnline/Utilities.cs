c5dc9a158988ae670b709d7f005968982172192a
Dennise Catolos <contact@dennise.me>
2020-09-15 16:46:42 +0800

    Updated internal code

---
+++ CommonToolsOnline/Utilities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace CommonToolsOnline
+{
+
+    public static class Utilities
+    {
+
+        public static string EncodeToBase64(string text)
+        {
+            var bytes = Encoding.UTF8.GetBytes(text);
+            return Uri.EscapeUriString(Convert.ToBase64String(bytes));
+        }
+
+        public static string DecodeFromBase64(string text)
+        {
+            var bytes = Convert.FromBase64String(Uri.UnescapeDataString(text));
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+    }
+
+}
