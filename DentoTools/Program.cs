9829be3887b32068eeb11ded09101ad4d5b16023
Dennise Catolos <63337204+dentolos19@users.noreply.github.com>
2021-04-24 09:24:29 +0800

    Project Refurbished

--- DentoTools/Program.cs
+++ DentoTools/Program.cs
@@ -3,18 +3,22 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Syncfusion.Blazor;
+using Syncfusion.Licensing;
 
 namespace DentoTools
 {
 
-    public static class Program
+    public class Program
     {
 
         public static async Task Main(string[] args)
         {
+            SyncfusionLicenseProvider.RegisterLicense("{LICENSE_KEY}"); 
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.RootComponents.Add<App>("#app");
+            builder.Services.AddSyncfusionBlazor();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             await builder.Build().RunAsync();
         }
 
--- DentoTools/wwwroot/assets/interop.js
+++
@@ -1,3 +0,0 @@
-﻿window.clipboardCopy = {
-    copyText: function (text) { navigator.clipboard.writeText(text); }
-};
